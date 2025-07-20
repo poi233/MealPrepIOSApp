ebf4c7019c0affb675545b367632e75be2c3e3e1
poi233 <593657039@qq.com>
2025-07-20 14:49:37 -0700

    Initial Commit

---
+++ MealPrepIOSApp/MealPrepIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPrepIOSAppApp.swift
+//  MealPrepIOSApp
+//
+//  Created by PuYihao on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MealPrepIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MealPrepIOSAppTests/MealPrepIOSAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MealPrepIOSAppTests.swift
+//  MealPrepIOSAppTests
+//
+//  Created by PuYihao on 7/20/25.
+//
+
+import Testing
+@testable import MealPrepIOSApp
+
+struct MealPrepIOSAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
